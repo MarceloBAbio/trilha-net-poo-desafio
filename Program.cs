d1ab6236630b89c671ec03796cb0d86c3d251ed8
Marcelo Barandela Abio <marceloabio20@gmail.com>
2022-09-10 02:13:47 -0300

    Classes e Testes implementados.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia.");
+Smartphone nokia = new Nokia("1234-5677", "Nokia", "1", 69);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Celular Iphone.");
+Smartphone iphone = new Iphone("8765-4321", "Iphone", "2", -69);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
